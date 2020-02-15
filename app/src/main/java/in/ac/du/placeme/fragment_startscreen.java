743009cc12a336258c615debb729b229a3451182
kinshuk-h <kinshukvasishtsmart@gmail.com>
2020-02-15 22:10:58 +0530

    UI Updates

---
+++ app/src/main/java/in/ac/du/placeme/fragment_startscreen.java
@@ -0,0 +1,14 @@
+package in.ac.du.placeme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fragment_startscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_startscreen);
+    }
+}
